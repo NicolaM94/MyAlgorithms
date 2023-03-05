0cc7597d8cf8e1931d661458ebd8eefe8e3cd5f9
NicolaM94 <nicola.moro2312@gmail.com>
2023-03-05 02:42:51 +0100

    implementing euler project problem 13 in main

--- main.go
+++ main.go
@@ -1,5 +1,28 @@
 package main
 
+import (
+	"fmt"
+	"os"
+)
+
 func main() {
 
+	file, err := os.ReadFile("numbers")
+	if err != nil {
+		panic(err)
+	}
+
+	container := []string{}
+	temp := ""
+
+	for _, v := range file {
+		if v == 10 {
+			container = append(container, temp)
+			temp = ""
+		}
+		temp += string(v)
+	}
+	container = append(container, temp)
+
+	fmt.Println(len(container))
 }
