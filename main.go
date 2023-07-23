6fa8a9bf3f2ca2f54367b41426d6842322ed2fbb
NicolaM94 <nicola.moro2312@gmail.com>
2023-07-23 18:17:36 +0200

    added insertion sort

--- main.go
+++ main.go
@@ -2,13 +2,17 @@ package main
 
 import (
 	"fmt"
-	orderingalgorithms "myalgo/orderingAlgorithms"
+	orderingalgorithms "myalgo/sorting"
 	
 )
 
 func main() {
 
-	array := []int{8,5,2,3,1,9}
+	array := []int{8,5,2,3,2,1,9}
 
+	fmt.Println("Selection sort")
 	fmt.Println(orderingalgorithms.SelectionSort(array))
+
+	fmt.Println("Insertion sort")
+	fmt.Println(orderingalgorithms.InsertionSort(array))
 }
--- sorting/subefficient.go
+++ sorting/subefficient.go
@@ -1,12 +1,12 @@
-package orderingalgorithms
+package sorting
 
 import (
 	"fmt"
 )
 
+// Selection sort algorithm
 func SelectionSort (array []int) []int {
 	for i:=0;i<=len(array)-2;i++ {
-		
 		minValue := i+1
 		for j:=i+2;j<len(array);j++ {
 			if array[j] < array[minValue] {
@@ -21,6 +21,20 @@ func SelectionSort (array []int) []int {
 		
 		fmt.Println(array)
 		
+	}
+	return array
+}
+
+// Insertion sort algorithm
+func InsertionSort (array []int) []int {
+	for i:=1;i<=len(array)-1;i++ {
+		for j := i-1;j==0;j-- {
+			if array[i] < array[j] {
+				temp := array[j]
+				array[j] = array[i]
+				array[i] = temp
+			}
+		}
 	}
 	return array
 }
