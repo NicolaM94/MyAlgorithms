01dae4a78522e3cdfc5ce34f1091573773b6c5af
NicolaM94 <nicola.moro2312@gmail.com>
2023-07-31 01:12:23 +0200

    Bug fixing

--- main.go
+++ main.go
@@ -1,15 +1,5 @@
 package main
 
-import (
-	"fmt"
-	orderingalgorithms "myalgo/sorting"
-)
-
 func main() {
 
-	array := []int{8, 5, 2, 3, 1, 9}
-
-	println(len(array))
-
-	fmt.Println(orderingalgorithms.BubbleSort(array))
 }
