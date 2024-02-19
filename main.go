a4ce6675658b712231db6b190870f7e98000ba75
NicolaM94 <nicola.moro2312@gmail.com>
2024-02-19 19:06:58 +0100

    Adding python file for possible solutions

--- main.go
+++ main.go
@@ -1,17 +1,5 @@
 package main
 
-import (
-	"fmt"
-	complexnumbers "myalgo/complexNumbers"
-)
-
 func main() {
-	a := complexnumbers.ComplexN{RealPart: 1, ImagPart: 1}
-	b := complexnumbers.ComplexN{RealPart: 2, ImagPart: 2}
-	a.PrintAlgebraic()
-	fmt.Println()
-	a.PrintTrigonometric()
-	fmt.Println()
-	b.PrintTrigonometric()
-	
+
 }
